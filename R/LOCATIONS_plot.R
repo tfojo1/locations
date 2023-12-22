37be85d1ec1fdce0ab4b4b5ae3ed42fde5534a0e
JPennington <jpennin6@jhu.edu>
2023-12-21 22:13:47 -0500

    First working version of state shading with ggmap

--- R/LOCATIONS_plot.R
+++ R/LOCATIONS_plot.R
@@ -1,10 +1,27 @@
+library(ggplot2)
+library(ggmap)
 
-#'@param data A data frame with a column labeled 'location'
-#'@param mapping Aesthetics to pass to the ggplot object, created by aes
-#'@param ... other arguments to pass to ggplot
+#'@title location.plot
+#'@description Create a plot of various points in the US located in a data frame with
+#'             an accompanying ggplot2 aesthetic (aes()).
+#'             
+#'@param data A data frame with a column labeled 'location', containing a location code 
+#'
+#'@param mapping Aesthetics to pass to the ggplot object, created by aes()
+#'
+#'@param title A title for the plotted area
+#'
+#'@export
 location.plot <- function(data,
                           mapping,
-                          ...)
+                          title=NA,
+                          poly.data=NA,
+                          size.range=c(1,5),
+                          color.range=c('blue', 'red'),
+                          pch=19,
+                          size.label='',
+                          color.label='',
+                          alpha=1)
 {
   # Check to make sure that 'data' is a data.frame
   if (!is.data.frame(data)) {
@@ -15,146 +32,116 @@ location.plot <- function(data,
     stop("No 'locations' column in the data.frame")
   }
   
+  # Get the coordinates for all the locations included in the data.frame; they
+  # are returned as a character value with two values separated by a comma.
+  # The first value is latitude, the second value is longitude
   coordinates = get.location.coords(data[['locations']])
-  xycoords = lapply(strsplit(coordinates, split=","), function (v) { v = as.numeric(v); return(list(x=v[1],y=v[2])) } )
   
-  ggmap(US.MAP) + geom_point(data=coordinates, mapping=xycords)
+  lat.lon = strsplit(coordinates, ",")
+  data["latitude"] = as.numeric(sapply(lat.lon, function(x) {return(x[1])}))
+  data["longitude"] = as.numeric(sapply(lat.lon, function(x) {return(x[2])}))
   
-    # # Set lon lat into mapping
-    # data$lon = x
-    # data$lat = y
-    # 
-    # mapping$x = lon
-    # mapping$y = lat
-    # 
-    # # A call to ggmap
-    # ggmap(US.MAP) + 
-    #   geom_point(data=data, mapping=mapping, ...)
-}
-
-add.lat.lon.to.data <- function(data)
-{
-    data$lon = x
-    data$lat = y
-    
-    data
-}
-name_data <- c('New York', 'Baltimore', 'Austin')
-locations_data <- c('C.35620', 'C.12580', 'C.12420')
-
-# Create the dataframe
-tdf <- data.frame(name=name_data, locations=locations_data)
-
-#-- THIS IS THE OLD CODE WHICH WE CAN BASE OFF OF --#
-
-library(ggplot2)
-library(ggmap)
-
-if (1==2)
-{
-  png(filename='../Manuscripts/manuscript_1/Annals Submission/video/map.png',
-      height = 1200, width=2400, res = 300)
-  plot.us.hiv.map(TARGET.MSAS) + theme(legend.position = 'none')
-  dev.off()
-}
-
-
-MAP.LABELS = c(
-  prevalence='HIV Prevalence',
-  new='Reported HIV Diagnoses'
-)
-
-plot.us.hiv.map <- function(locations,
-                            surv=msa.surveillance,
-                            size.by='new',
-                            size.by.year=2018,
-                            size.range=c(3,7),
-                            color.by=NULL,
-                            color.by.year=2018,
-                            color.range='red',
-                            size.label=paste0(size.by.year, ' ', MAP.LDABELS[size.by]),
-                            color.label=paste0(color.by.year, ' ', MAP.LABELS[size.by]),
-                            alpha=0.5
-)
-{
-  lat.long = get.location.latitude.longitude(locations)
-  if (is.null(size.by))
-    size = size.range
-  else
-    size = get.surveillance.data(surv, location.codes = locations, data.type=size.by, years=size.by.year,
-                                 aggregate.locations = F, aggregate.years=T)
+  # Are any of the locations NA?  This is a result of missing location data.
+  na.indexes = which(is.na(data["latitude"]))
   
-  if (is.null(color.by))
-    color = color.range
-  else
-    color = get.surveillance.data(surv, location.codes = locations, data.type=color.by, years=color.by.year,
-                                  aggregate.locations = F, aggregate.years=T)
+  # If there are any NA locations, display a list
+  if (length(na.indexes) > 0) {
+    cat(paste0("Unable to plot ", length(na.indexes)," locations, no location data :\n"))
+    for (i in seq_along(na.indexes)) {
+      # We could print the name in those dataframes that contain it, but all we know for 
+      # sure is that this dataframe contains a locations column
+      cat(paste0(i, ". ", data[["locations"]][na.indexes[i]],"\n"))
+    }
+  }
   
-  plot.us.map(latitude = lat.long$latitude,
-              longitude = lat.long$longitude,
-              size=size,
-              color=color,
-              size.range = if(is.null(size.by)) NULL else size.range,
-              color.range= if(is.null(color.by)) NULL else color.range,
-              size.label = size.label,
-              color.label = color.label,
-              alpha=alpha)
+  
+  state.data = poly.data [ poly.data$NAME %in% c("Washington","Florida", "Wyoming"), ]
+  # state.data = poly.data
+  
+  plot = ggmap(US.MAP) +
+    geom_point(data=data, mapping, shape = pch, alpha = alpha) +
+    geom_polygon(data = state.data, aes(x = longitude, y = latitude, group=poly), fill = "cyan", color = "black", alpha = 1.0) +
+    theme(panel.background = element_rect(fill='white'), 
+          axis.ticks = element_blank(),
+          axis.text = element_blank(), 
+          axis.title = element_blank(), 
+          plot.title = element_text(hjust = 0.5))
+  
+  
+  # Add a title if asked for
+  if ( !is.na(title) ) {
+    plot = plot + ggtitle(title)
+  }
+  
+  if (!is.null(size.range)){
+    plot = plot + scale_size_continuous(name=size.label, range=size.range)
+  }
+  
+  if (!is.null(color.range)) {
+    plot = plot + scale_fill_gradient(name=color.label, low=color.range[1], high=color.range[2])
+  } 
+  
+  plot
 }
 
 ##--------------------------------------------------##
 ##-- THE GENERAL PLOT FUNCTION AND NECESSARY DATA --##
 ##--------------------------------------------------##
-
+register_stadiamaps(api_key)
 #US.MAP = get_stamenmap(bbox=c(left=-125,bottom=24, right=-66, top=50),zoom=4, maptype='toner-background')
-#attr_map <- attr(US.MAP, "bb")    # save attributes from original
+US.MAP = get_stadiamap(bbox=c(left=-125,bottom=24,right=-66, top=50), zoom = 5, maptype = "stamen_toner_background")
 
-## change color in raster
-#US.MAP[US.MAP == "#000000"] <- "#C0C0C0"
+# ALL ON BEFORE JP
+attr_map <- attr(US.MAP, "bb")    # save attributes from original
+# 
+# ## change color in raster; change the black background to a nicer gray
+US.MAP[US.MAP == "#000000"] <- "#C0C0C0"
+# Some background is colored with almost-black, change it as well
+US.MAP[US.MAP == "#010101"] <- "#C0C0C0"
+# 
+# ## correct class, attributes
+class(US.MAP) <- c("ggmap", "raster")
+attr(US.MAP, "bb") <- attr_map
 
-## correct class, attributes
-#class(US.MAP) <- c("ggmap", "raster")
-#attr(US.MAP, "bb") <- attr_map
+# Load the state polygon data
+poly.data = read.csv("data-raw/geom_data.csv", stringsAsFactors = FALSE)
 
-#plot.us.map <- function(latitude,
-                        #longitude,
-                        #size=1,
-                        #color=1,
-                        #size.range=c(1,5),
-                        #color.range=c('blue', 'red'),
-                        #pch=21,
-                        #size.label='',
-                        #color.label='',
-                        #alpha=1)
-#{
-  #df = data.frame(lon=longitude,
-                  #lat=latitude,
-                  #size=size,
-                  #color=color)
+# Initialize the "poly" column
+poly.data$poly <- rep(1, nrow(poly.data))
+
+# Variables to store the first point of the first polygon in each state
+first_lat <-  poly.data$latitude[1]
+first_long <- poly.data$longitude[1]
+poly.index <- 1
+poly.reset = FALSE
+
+# Iterate through rows to increment "poly" when a new polygon starts
+# Here we are counting polygons in the set; some states have multiple polygons
+# and geom_polygon has a group= feature that allows you to group by polygon.
+# So we are numbering the polygons here
+for (i in 2:nrow(poly.data)) {
+  poly.data$poly[i] = poly.index
   
-  #rv = ggmap(US.MAP) + 
-    #geom_point(data=df, aes(lon, lat, size=size, fill=color), shape=pch, alpha=alpha) +
-    #theme(panel.background = element_rect(fill='white'), axis.ticks = element_blank(), 
-          #axis.text = element_blank(), axis.title = element_blank())
+  if (poly.reset) {
+    first_lat = poly.data$latitude[i]
+    first_long = poly.data$longitude[i]
+  }
   
-  #if (!is.null(size.range))
-    #rv = rv + scale_size_continuous(name=size.label, range=size.range)
-  #else
-  #{
-    #sizes = unique(df$size)
-    #names(sizes) = sizes
-    
-    #rv = rv + scale_size_manual(guide='none', values=sizes)
-  #}
-  
-  #if (!is.null(color.range))
-    #rv = rv + scale_fill_gradient(name=color.label, low=color.range[1], high=color.range[2])
-  #else
-  #{
-    #colors = unique(df$color)
-    #names(colors) = colors
-    
-    #rv = rv + scale_fill_manual(guide='none', values=colors)
-  #}
-  
-  #rv
-#}
+  if (poly.data$latitude[i] == first_lat && poly.data$longitude[i] == first_long && !poly.reset) {
+    #This is the end of a polygon
+    poly.index = poly.index + 1
+    poly.reset = TRUE
+  } else {
+    poly.reset = FALSE
+  }
+}
+
+# Test plot: State CBSAs
+state.cbsa = get.contained.locations("TX","CBSA")
+state2.cbsa = get.contained.locations("OH", "CBSA")
+name_data = c(names(state.cbsa),names(state2.cbsa))
+code_data = c(unname(state.cbsa),unname(state2.cbsa))
+
+state.df = data.frame(locations=code_data, size=rep(1,length(code_data)), color=seq(1,length(code_data)))
+
+location.plot(state.df,aes(x=longitude, y=latitude,size=size,color=color), "State CBSAs", poly.data)
